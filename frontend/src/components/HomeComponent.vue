b9b61533850df6d9c1ff91f599a84f796bdd469b
daniel <danielmatheus@poli.ufrj.br>
2023-05-30 21:21:46 -0300

    commit daniel

--- frontend/src/components/HomeComponent.vue
+++ frontend/src/components/HomeComponent.vue
@@ -3,7 +3,7 @@
     <v-responsive class="d-flex align-center text-center fill-height">
       <v-img height="300" src="@/assets/logo.svg" />
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+      <div class="text-body-2 font-weight-light mb-n1">Welcome to !!!!!!!!</div>
 
       <h1 class="text-h2 font-weight-bold">Vuetify</h1>
 
