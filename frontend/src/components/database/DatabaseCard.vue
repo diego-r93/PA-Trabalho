16b05cf357fcfc7b9c6558a090d0301aa9b40f3d
diego <diegorodrigues-5@hotmail.com>
2023-06-20 01:19:47 -0300

    Configurando README

--- frontend/src/components/database/DatabaseCard.vue
+++ frontend/src/components/database/DatabaseCard.vue
@@ -1,61 +1,61 @@
 <template>
-   <v-card>
-      <v-container class="buttons-container">
-        <v-btn  class="delete-btn" @click="deleteOverlay = true">X</v-btn>
-      <v-btn  class="edit-btn" @click="editOverlay = true" icon="mdi-pencil" size="x-small"></v-btn>
-      </v-container>
-    
+  <v-card>
+    <v-container class="buttons-container">
+      <v-btn class="delete-btn" @click="deleteOverlay = true">X</v-btn>
+      <v-btn class="edit-btn" @click="editOverlay = true" icon="mdi-pencil" size="x-small"></v-btn>
+    </v-container>
+
     <v-list>
-        <v-list-item>
-          <v-list-item-title>{{ cardData.pumperCode }} / {{ cardData.pumperName }} </v-list-item-title>
-          <v-divider class="device-card-divider"></v-divider>
+      <v-list-item>
+        <v-list-item-title>{{ cardData.pumperCode }} / {{ cardData.pumperName }} </v-list-item-title>
+        <v-divider class="device-card-divider"></v-divider>
 
-          <v-list-item-subtitle class="device-property">
-            <v-icon>mdi-circle-small</v-icon>
-            Pulse Time: {{ cardData.pulseDuration }}
-          </v-list-item-subtitle>
+        <v-list-item-subtitle class="device-property">
+          <v-icon>mdi-circle-small</v-icon>
+          Pulse Time: {{ cardData.pulseDuration }}
+        </v-list-item-subtitle>
 
-          <v-list-item-subtitle class="device-property">
-            <v-icon>mdi-circle-small</v-icon>
-            Created At: {{ cardData.createdAt }}
-          </v-list-item-subtitle>
+        <v-list-item-subtitle class="device-property">
+          <v-icon>mdi-circle-small</v-icon>
+          Created At: {{ cardData.createdAt }}
+        </v-list-item-subtitle>
 
-          <v-list-item-subtitle class="device-property">
-            <v-icon>mdi-circle-small</v-icon>
-            Updated At: {{ cardData.updatedAt }}
-          </v-list-item-subtitle> 
+        <v-list-item-subtitle class="device-property">
+          <v-icon>mdi-circle-small</v-icon>
+          Updated At: {{ cardData.updatedAt }}
+        </v-list-item-subtitle>
 
-          <v-expansion-panels variant="default">
-            <v-expansion-panel title="Drive Times">
-              <v-expansion-panel-text>
-                <v-list v-if="hasDriveTimes">
-                  <v-list-item v-for="driveTime in cardData.driveTimes" :key="driveTime">
-                    <v-list-item-subtitle>
-                      <v-icon>mdi-square-small</v-icon>
-                      Time: {{ driveTime.time }}/ State:{{ driveTime.state }}
-                    </v-list-item-subtitle>
-                  </v-list-item>
-                </v-list>
-              </v-expansion-panel-text>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </v-list-item>
+        <v-expansion-panels variant="default">
+          <v-expansion-panel title="Drive Times">
+            <v-expansion-panel-text>
+              <v-list v-if="hasDriveTimes">
+                <v-list-item v-for="driveTime in cardData.driveTimes" :key="driveTime">
+                  <v-list-item-subtitle>
+                    <v-icon>mdi-square-small</v-icon>
+                    Time: {{ driveTime.time }}/ State:{{ driveTime.state }}
+                  </v-list-item-subtitle>
+                </v-list-item>
+              </v-list>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-list-item>
     </v-list>
   </v-card>
   <v-overlay v-model="deleteOverlay" class="d-flex align-center justify-center">
     <v-card class="text-center delete-confirmation-card">
-    <v-card-title>
-      Are you sure you want to delete this board?
-    </v-card-title>
+      <v-card-title>
+        Are you sure you want to delete this board?
+      </v-card-title>
       <v-row class="d-flex align-center justify-center">
-          <v-col cols="12" sm="6" md="4">
-            <v-btn block rounded="0" size="x-large" @click="deleteOverlay=false" color="red">No</v-btn>
-          </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-btn block rounded="0" size="x-large" @click="deleteOverlay = false" color="red">No</v-btn>
+        </v-col>
 
-          <v-col cols="12" sm="6" md="4">
-            <v-btn block rounded="xs" size="x-large" @click="deleteBoard" color="green">Yes</v-btn>
-          </v-col>
-        </v-row>
+        <v-col cols="12" sm="6" md="4">
+          <v-btn block rounded="xs" size="x-large" @click="deleteBoard" color="green">Yes</v-btn>
+        </v-col>
+      </v-row>
     </v-card>
   </v-overlay>
   <v-overlay v-model="editOverlay" class="d-flex align-center justify-center">
@@ -67,20 +67,20 @@
         <v-text-field v-model="pumperCode" label="Pumper Code" :rules="[required]"></v-text-field>
         <v-text-field v-model="pumperName" label="Pumper Name" :rules="[required]"></v-text-field>
         <v-text-field type="number" v-model="pulseDuration" label="Pulse Duration" :rules="[required]"></v-text-field>
-        
+
         <v-virtual-scroll :height="300" :items="this.driveTimes">
-        <template v-slot:default="{ item }">
-          <v-container>
+          <template v-slot:default="{ item }">
+            <v-container>
               <v-divider></v-divider>
             </v-container>
-              <v-text-field v-model="item.time" label="Time"></v-text-field>
-              <v-checkbox v-model="item.state" label="State = true"></v-checkbox>
-        </template>
+            <v-text-field v-model="item.time" label="Time"></v-text-field>
+            <v-checkbox v-model="item.state" label="State = true"></v-checkbox>
+          </template>
         </v-virtual-scroll>
 
         <v-row class="d-flex align-center justify-center">
           <v-col cols="12" sm="6" md="4">
-            <v-btn  class="add-drive-time-btn" @click="addDriveTime">Add <br> Drive Time</v-btn>
+            <v-btn class="add-drive-time-btn" @click="addDriveTime">Add <br> Drive Time</v-btn>
           </v-col>
 
           <v-col cols="12" sm="6" md="4">
@@ -94,7 +94,9 @@
           </v-col>
 
           <v-col cols="12" sm="6" md="4">
-            <v-btn :loading="loading" block rounded="xs" size="x-large"  :disabled="this.pumperCode.length === 0 || this.pumperName.length === 0 || this.pulseDuration.length === 0" @click="editBoard">Submit</v-btn>
+            <v-btn :loading="loading" block rounded="xs" size="x-large"
+              :disabled="this.pumperCode.length === 0 || this.pumperName.length === 0 || this.pulseDuration.length === 0"
+              @click="editBoard">Submit</v-btn>
           </v-col>
         </v-row>
       </v-form>
@@ -103,98 +105,99 @@
 </template>
 
 <script>
-  import mongoDataService from "@/services/mongoDataService"
-  export default {
-    name: 'DatabaseCard',
-    props: {
-        cardData: Object
+import mongoDataService from "@/services/mongoDataService"
+export default {
+  name: 'DatabaseCard',
+  props: {
+    cardData: Object
+  },
+  methods: {
+    editBoard() {
+      mongoDataService.updateBoard(this.cardData.id, {
+        pumperCode: this.pumperCode,
+        pumperName: this.pumperName,
+        pulseDuration: this.pulseDuration,
+        driveTimes: this.driveTimes
+      }).then((response) => {
+        alert("Board successfuly updated!")
+        window.location.reload()
+      })
     },
-    methods: {
-        editBoard() {
-          mongoDataService.updateBoard(this.cardData.id, {
-            pumperCode: this.pumperCode,
-            pumperName: this.pumperName,
-            pulseDuration: this.pulseDuration,
-            driveTimes: this.driveTimes
-          }).then((response) => {
-            alert("Board successfuly updated!")
-            window.location.reload()
-          })
-        },
-        deleteBoard() {
-          mongoDataService.deleteBoard(this.cardData.id).then((response) => {
-            alert("Board successfully deleted!")
-            window.location.reload();
-          })
-        },
-        addDriveTime() {
-          this.driveTimes.push({
-            time: "",
-            state: false,
-          });
-          console.log(this.driveTimes)
-        },
-        removeDriveTime() {
-          this.driveTimes.pop()
-        },
-        required(v) {
-          return !!v || 'Field is required'
-        }
+    deleteBoard() {
+      mongoDataService.deleteBoard(this.cardData.id).then((response) => {
+        alert("Board successfully deleted!")
+        window.location.reload();
+      })
     },
-    data() {
-      return {
-        deleteOverlay:false,
-        editOverlay: false,
-        pumperCode: "",
-        pumperName:"",
-        pulseDuration:"",
-        driveTimes: [],
-        form: false,
-        loading:false
-      }
+    addDriveTime() {
+      this.driveTimes.push({
+        time: "",
+        state: false,
+      });
+      console.log(this.driveTimes)
     },
-    created() {
-      this.pumperCode = this.cardData.pumperCode;
-      this.pumperName = this.cardData.pumperName;
-      this.pulseDuration = this.cardData.pulseDuration;
-      this.driveTimes = this.cardData.driveTimes;
-      console.log("dkgnosngosgo")
+    removeDriveTime() {
+      this.driveTimes.pop()
     },
-    computed: {
-      hasDriveTimes() {
-        return this.cardData.driveTimes.length > 0 ;
-      }
+    required(v) {
+      return !!v || 'Field is required'
     }
-  };
+  },
+  data() {
+    return {
+      deleteOverlay: false,
+      editOverlay: false,
+      pumperCode: "",
+      pumperName: "",
+      pulseDuration: "",
+      driveTimes: [],
+      form: false,
+      loading: false
+    }
+  },
+  created() {
+    this.pumperCode = this.cardData.pumperCode;
+    this.pumperName = this.cardData.pumperName;
+    this.pulseDuration = this.cardData.pulseDuration;
+    this.driveTimes = this.cardData.driveTimes;
+    console.log("dkgnosngosgo")
+  },
+  computed: {
+    hasDriveTimes() {
+      return this.cardData.driveTimes.length > 0;
+    }
+  }
+};
 </script>
 
 <style scoped>
-  .device-property {
-    margin-bottom: 2px;
-  }
-  .device-card-divider {
-    margin-bottom: 10px;
-  }
+.device-property {
+  margin-bottom: 2px;
+}
 
-  .v-card {
-    position: relative
-  }
+.device-card-divider {
+  margin-bottom: 10px;
+}
 
-  .delete-btn {
-    position: absolute;
-    top: 0;
-    right: 0;
-    z-index: 1;
-    box-shadow: none;
-  }
+.v-card {
+  position: relative
+}
 
-  .edit-btn{
-    position: absolute;
-    top: 0;
-    right: left;
-    z-index: 1;
-    box-shadow: none;
-  }
+.delete-btn {
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 1;
+  box-shadow: none;
+}
+
+.edit-btn {
+  position: absolute;
+  top: 0;
+  right: left;
+  z-index: 1;
+  box-shadow: none;
+}
 
 .delete-confirmation-card {
   padding: 50px;
@@ -205,5 +208,4 @@
   padding: 20px;
   width: 80vh;
 }
-
 </style>
--- frontend/src/components/database/DatabaseCardList.vue
+++ frontend/src/components/database/DatabaseCardList.vue
@@ -2,17 +2,10 @@
   <v-container class="container">
     <v-row>
       <v-col v-for="(device, index) in devices" :key="index" cols="6">
-        <DatabaseCard :cardData="device"/>
+        <DatabaseCard :cardData="device" />
       </v-col>
     </v-row>
-    <v-btn
-      fab
-      dark
-      class="btn-float"
-      icon="mdi-plus"
-      size="large"
-      @click="addBoardFormIsVisible = true"
-    >
+    <v-btn color="white" fab dark class="btn-float" icon="mdi-plus" size="large" @click="addBoardFormIsVisible = true">
     </v-btn>
   </v-container>
   <v-overlay v-model="addBoardFormIsVisible" class="d-flex align-center justify-center">
@@ -24,20 +17,20 @@
         <v-text-field v-model="pumperCode" label="Pumper Code" :rules="[required]"></v-text-field>
         <v-text-field v-model="pumperName" label="Pumper Name" :rules="[required]"></v-text-field>
         <v-text-field type="number" v-model="pulseDuration" label="Pulse Duration" :rules="[required]"></v-text-field>
-        
+
         <v-virtual-scroll :height="300" :items="this.driveTimes">
-        <template v-slot:default="{ item }">
-          <v-container>
+          <template v-slot:default="{ item }">
+            <v-container>
               <v-divider></v-divider>
             </v-container>
-              <v-text-field v-model="item.time" label="Time"></v-text-field>
-              <v-checkbox v-model="item.state" label="State = true"></v-checkbox>
-        </template>
+            <v-text-field v-model="item.time" label="Time"></v-text-field>
+            <v-checkbox v-model="item.state" label="State = true"></v-checkbox>
+          </template>
         </v-virtual-scroll>
 
         <v-row class="d-flex align-center justify-center">
           <v-col cols="12" sm="6" md="4">
-            <v-btn  class="add-drive-time-btn" @click="addDriveTime">Add <br> Drive Time</v-btn>
+            <v-btn class="add-drive-time-btn" @click="addDriveTime">Add <br> Drive Time</v-btn>
           </v-col>
 
           <v-col cols="12" sm="6" md="4">
@@ -51,7 +44,8 @@
           </v-col>
 
           <v-col cols="12" sm="6" md="4">
-            <v-btn :loading="loading" block rounded="xs" size="x-large" @click="addNewBoard" :disabled="this.pumperCode.length === 0 || this.pumperName.length === 0 || this.pulseDuration.length === 0">Submit</v-btn>
+            <v-btn :loading="loading" block rounded="xs" size="x-large" @click="addNewBoard"
+              :disabled="this.pumperCode.length === 0 || this.pumperName.length === 0 || this.pulseDuration.length === 0">Submit</v-btn>
           </v-col>
         </v-row>
       </v-form>
@@ -64,6 +58,7 @@
   min-height: 100vh;
   position: relative;
 }
+
 .btn-float {
   position: absolute;
   bottom: 0;
@@ -79,64 +74,62 @@
 .add-drive-time-btn {
   margin-bottom: 10px
 }
-
-
 </style>
 
 <script>
 import DatabaseCard from './DatabaseCard.vue'
 import mongoDataService from "@/services/mongoDataService"
-  export default {
-    name: "DatabaseCardList",
-    components: {
-      DatabaseCard
-    },
-    data: () => ({
-        devices: [],
-        addBoardFormIsVisible: false,
-        pumperCode: "",
-        pumperName:"",
-        pulseDuration:"",
-        driveTimes: [],
-        form: false,
-        loading:false
-    }),
-    methods: {
-      addNewBoard() {
-        this.loading=true
-        mongoDataService.createBoard({
-          pumperCode: this.pumperCode,
-          pumperName: this.pumperName,
-          pulseDuration: this.pulseDuration,
-          driveTimes: this.driveTimes,
-        }).then(response => {
+export default {
+  name: "DatabaseCardList",
+  components: {
+    DatabaseCard
+  },
+  data: () => ({
+    devices: [],
+    addBoardFormIsVisible: false,
+    pumperCode: "",
+    pumperName: "",
+    pulseDuration: "",
+    driveTimes: [],
+    form: false,
+    loading: false
+  }),
+  methods: {
+    addNewBoard() {
+      this.loading = true
+      mongoDataService.createBoard({
+        pumperCode: this.pumperCode,
+        pumperName: this.pumperName,
+        pulseDuration: this.pulseDuration,
+        driveTimes: this.driveTimes,
+      }).then(response => {
         alert("Nova placa adicionada com sucesso!")
-        this.loading=false
+        this.loading = false
         window.location.reload()
-        })
-      },
-      addDriveTime() {
-        this.driveTimes.push({
-          time: "",
-          state: false,
-        });
-        console.log(this.driveTimes)
-      },
-      removeDriveTime() {
-        this.driveTimes.pop()
-      },
-      required(v) {
-        return !!v || 'Field is required'
-      }
+      })
     },
-    created() {
-      try {
-        mongoDataService.getAllBoards().then((response) => {
-          this.devices = response.data
-        })
-      } catch (error) {
-        alert(error)
-      }
+    addDriveTime() {
+      this.driveTimes.push({
+        time: "",
+        state: false,
+      });
+      console.log(this.driveTimes)
+    },
+    removeDriveTime() {
+      this.driveTimes.pop()
+    },
+    required(v) {
+      return !!v || 'Field is required'
     }
+  },
+  created() {
+    try {
+      mongoDataService.getAllBoards().then((response) => {
+        this.devices = response.data
+      })
+    } catch (error) {
+      alert(error)
+    }
+  }
 }
 </script>
